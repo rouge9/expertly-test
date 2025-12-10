b6e944808541a997eebe7c1bd9b1a7353ad0a14f
rouge9 <robelged1992@gmail.com>
2025-12-10 12:36:39 +0300

    inint setup commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Click me {count}
+      </Button>
+    </>
+  );
+}
+
+export default App;
